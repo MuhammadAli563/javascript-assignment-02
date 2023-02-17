5400311589125f46a74c2dc69124576efef4e5bf
Muhammad Ali <03152214619ali@gmail.com>
2023-02-17 09:42:06 +0500

    jaascript

---
+++ app.js
@@ -0,0 +1,166 @@
+// CHAPTER #01
+// Q#01
+// alert("Error! Please Enter A Valid Password")
+
+// Q#02
+// document.write("Error! Please Enter A Valid Password")
+
+// Q#03
+// alert("Welcome To JS Land\nHappy Coding!")
+
+// Q#04
+// alert("Welcome To JS Land")
+// alert("Happy Coding!")
+
+// Q#05
+// console.log("Hello i can run JS through web browser's console")
+
+
+
+// CHAPTER # 02
+// Q#01
+// var username = "username";
+
+// Q#02
+// var myname = "Muhammad Ali"
+
+// Q#03
+// var message = "Hello World";
+// alert(message);
+
+// Q#04
+// var name = "Jhon Doe"
+// var age = 15;
+// var degree = "certified mobile application development"
+// alert(name);
+// alert(age);
+// alert(degree);
+
+// Q#05
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// Q#06
+// var email = "ahplay786@gmail.com"
+// alert("my email adress is " + email);
+
+// Q#07
+// var book = "a smarter way to learn javascript"
+// alert("im trying to learn from the book " + book);
+
+// Q#08
+// document.write("yeah i can write HTML content through javascript ");
+
+// Q#09
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+
+
+// CHAPTER #03
+// Q#01
+// var age = 21;
+// alert(age);
+
+// Q#02
+// ("UNKONWN") 
+
+// Q#03
+// var birthyear = 2001;
+// document.write("my birth yeaar is " + birthyear + " data type of my declared variable is number");
+
+// Q#04
+// var name = prompt('Enter Your Name');
+// var product = prompt('Enter Your Product');
+// var quantity = +prompt('How Many You Want To Buy?');
+// document.write(name + " Ordered " + quantity + " " + " Pieces Of " + product + " From our website " );
+
+
+
+// CHAPTER #04
+// Q#01
+// var name = "Ali",
+//     age = 21,
+//     height = "5'6"
+
+// Q#02
+// var 213name = "ali"
+// var name = "Ali"
+
+// Q#03
+// var str = "Rules for naming JS variables ";
+// document.write(str.bold());
+// document.write("Variable names can only contain numbers $ and _ . For example $my_1stVariable ");
+// document.write("Variables must begin with a numbers $ and _. For example $name, _name or name ");
+// document.write("Variable names are case sensitive ");
+// document.write("Variable names should not be JS keywords ");
+ 
+
+
+// CHAPTER #05
+// Q#01
+// var a = 5
+// var b = 10
+// var sum = a + b
+// console.log(sum);
+
+// Q#02
+// var a = 5
+// var b = 10
+// var sum = a - b
+// console.log(sum);
+// var a = 5
+// var b = 10
+// var sum = a / b
+// console.log(sum);
+// var a = 5
+// var b = 10
+// var sum = a * b
+// console.log(sum);
+
+// Q#03
+// var text = "Value after variable declaration is ??"
+// document.write(text);
+// var a = 10
+// document.write("initial value " + a)
+
+// Q#04
+// var tp = 600
+// var q = 5
+// document.write("total cost of buying " + q + " tickets price is " + tp)
+
+// Q#05
+// var table = +prompt('Enter an integer: ')
+// for(var i = 1; i <= 10; i++) 
+// {
+//     var result = i * table;
+//     console.log(`${table} * ${i} = ${result}`)
+// }
+
+
+
+// CHAPTER #06-09
+// Q#01
+// skip
+
+// Q#02
+// SKIP
+
+// Q#03
+// var name = prompt("Hello There!")
+
+// Q#05
+// for(var i = 5; i <= 10; i++) 
+// {
+//     var result = i * table;
+//     console.log(`${table} * ${i} = ${result}`)
+// }
+// var table = +prompt('Enter an integer: ')
+// for(var i = 1; i <= 10; i++) 
+// {
+//     var result = i * table;
+//     console.log(`${table} * ${i} = ${result}`)
+// }
+
+// Q#06
+// SKIP
+
